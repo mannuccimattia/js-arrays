85546453de7ef2a1a2200d3ff392a124bf7b1c0a
mannuccimattia <mannuccimattia@gmail.com>
2025-03-19 15:13:59 +0100

    [UPD] Done arrays - 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,22 +11,25 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-//stampa di controllo
+// stampa di controllo
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers.splice(4, 1, "Patrick");
-//stampa di controllo
+// stampa di controllo
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers[teachers.length-1];
 teachers.pop();
-//stampa di controllo
+// stampa di controllo
 console.log(lastTeacher, teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+teachers.shift();
+// stampa di controllo
+console.log(firstTeacher, teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
