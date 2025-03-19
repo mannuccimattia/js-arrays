db8cc8c715d3a92c861448ba58ab528d62fac09a
mannuccimattia <mannuccimattia@gmail.com>
2025-03-19 15:14:53 +0100

    [UPD] Done arrays - 5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -32,6 +32,9 @@ teachers.shift();
 console.log(firstTeacher, teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+// stampa di controllo
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
